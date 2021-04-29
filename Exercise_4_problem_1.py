f1b2c667ec603805949ca0162b9b51c678369098
aaakbw <>
2021-04-29 00:13:16 +0000

    I created a function called fahr_to_celsius.

--- Exercise_4_problem_1.py
+++ Exercise_4_problem_1.py
@@ -19,12 +19,18 @@
 # - Adding comments in your code and a docstring that explains how to use your `fahr_to_celsius` function (i.e., you should write the purpose of the function, parameters, and returned values)
 
 # YOUR CODE HERE
+def fahr_to_celsius(temp_fahrenheit):
+  """Convert from Fahrenheit to Celsius"""
+  converted_temp =(temp_fahrenheit-32)/1.8
+  return converted_temp 
 
 # ### Problem 1 tests
 # 
 # Check that the function produces correct answers for:
 # 1. What is 48° Fahrenheit in Celsius? 
+print(fahr_to_celsius(48))
 # 2. What about 71° Fahrenheit in Celsius?
+print(fahr_to_celsius(71))
 
 # ### Check your code
 # 
@@ -42,8 +48,8 @@
 # Write your answers below:
 
 # YOUR ANSWER HERE. Write your answers as comments
-#
-#
+#1. yes
+#2.　We can make the code concise.
 #
 #
 
